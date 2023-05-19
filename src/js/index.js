937fd235030e73e86906dae808db01656a9179fb
Glauco Morera <gmorerafortes@gmail.com>
2023-05-18 23:15:09 -0300

    Criação dos arquivos JavaScript

---
+++ src/js/index.js
@@ -0,0 +1,48 @@
+import { user } from '/src/js/services/user.js'
+import { repositories } from '/src/js/services/repositories.js'
+
+document.getElementById('btn-search').addEventListener('click', () => {
+    const userName = document.getElementById('input-search').value
+    getUserProfile(userName)
+})
+
+document.getElementById('input-search').addEventListener('keyup', (e) => {
+    const userName = e.target.value
+    const key = e.which || e.keyCode
+    const isEnterKeyPressed = key === 13
+
+    if (isEnterKeyPressed) {
+        getUserProfile(userName)
+    }
+})
+
+function getUserProfile(userName) {
+    user(userName).then(userData => {
+        let userInfor = `<div class="info">
+                            <img src="${userData.avatar_url}" alt="Foto do perfil do usuário" />
+                            <div class="data">
+                                <h1>${userData.name ?? 'Não possui nome cadastrado 😢'}</h1>
+                                <p>${userData.bio ?? 'Não possui bio cadastrada 😢'}</p>  
+                            </div> 
+                        </div>`
+
+        document.querySelector('.profile-data').innerHTML = userInfor
+
+        getUserRepositories(userName)
+    })
+}
+
+function getUserRepositories(userName) {
+    repositories(userName).then(reposData => {
+        let respositoriesItens = ""
+
+        reposData.forEach(repo => {
+            respositoriesItens += `<li><a href="${repo.html_url} target="_blank">${repo.name}</a></li>`
+        })
+
+        document.querySelector('.profile-data').innerHTML += `<div class="repositories section">
+                                                                <h2>Repositórios</h2>
+                                                                <ul>${respositoriesItens}</ul>
+                                                              </div>`
+    })
+}
